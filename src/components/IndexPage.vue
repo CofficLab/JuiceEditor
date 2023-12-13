f603549af4de850f8e30f85b4ca4431b0917aeaf
nookery <nooks@qq.com>
2023-12-13 18:09:00 +0800

    Update IndexPage.vue

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -2,12 +2,20 @@
   <div class="flex flex-col h-full w-full relative overflow-scroll">
     <!-- 操作栏 -->
     <div class="flex flex-row container mx-auto justify-end mt-4 sticky top-4 join" v-if="featureStore.toolbarVisible">
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableEdit">编辑模式</button>
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableEdit">只读模式</button>
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableTable">开启 Table</button>
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableTable">关闭 Table</button>
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableDraw">开启 Draw</button>
-      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableDraw">关闭 Draw</button>
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableEdit"
+        v-if="!featureStore.editable">编辑模式</button>
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableEdit"
+        v-if="featureStore.editable">只读模式</button>
+
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableTable"
+        v-if="!featureStore.tableEnabled">开启 Table</button>
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableTable"
+        v-if="featureStore.tableEnabled">关闭 Table</button>
+
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.enableDraw"
+        v-if="!featureStore.drawEnabled">开启 Draw</button>
+      <button class="btn btn-primary btn-xs join-item" @click="featureStore.disableDraw"
+        v-if="featureStore.drawEnabled">关闭 Draw</button>
     </div>
 
     <!-- loading -->
