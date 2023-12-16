eb8154ada70aced8d36235a670bda4a9b5c14eeb
nooks <nooks@qq.com>
2023-12-16 14:41:19 +0800

    优化表格的弹出菜单

--- src/extensions/SmartTable/SmartTable.vue
+++ src/extensions/SmartTable/SmartTable.vue
@@ -2,42 +2,44 @@
   <node-view-wrapper>
     <!-- 表格的操作 -->
     <template v-if="shouldShow">
-      <div class="table-menus" contenteditable="false">
-        <button @click="editor.chain().focus().addColumnBefore().run()">
-          <img src="../../assets/table.column.plus.before.svg" alt="" class="m-0 w-5 h-5" />
-        </button>
-        <button @click="editor.chain().focus().addColumnAfter().run()">
-          <img src="../../assets/table.column.plus.after.svg" alt="" class="m-0 w-5 h-5" />
-        </button>
-        <button @click="editor.chain().focus().deleteColumn().run()">
-          <img src="../../assets/table.column.remove.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().addRowBefore().run()">
-          <img src="../../assets/table.row.plus.before.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().addRowAfter().run()">
-          <img src="../../assets/table.row.plus.after.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().deleteRow().run()">
-          <img src="../../assets/table.row.remove.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().deleteTable().run()">
-          <img src="../../assets/table.remove.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().mergeCells().run()">
-          <img src="../../assets/table.cell.merge.svg" alt="" class="m-0 w-6 h-6" />
-        </button>
-        <button @click="editor.chain().focus().splitCell().run()">
-          <img src="../../assets/table.cell.split.svg" alt="" class="w-6 h-6 m-0" />
-        </button>
-        <button @click="props.editor.chain().focus().toggleHeaderColumn().run()">切换表头列</button>
-        <button @click="editor.chain().focus().toggleHeaderRow().run()">切换表头行</button>
-        <button @click="editor.chain().focus().toggleHeaderCell().run()">切换表头格</button>
-        <button @click="editor.chain().focus().mergeOrSplit().run()">合并或拆分</button>
-        <button @click="editor.chain().focus().setCellAttribute('colspan', 2).run()">占两格</button>
-        <button @click="editor.chain().focus().fixTables().run()">修复</button>
-        <button @click="editor.chain().focus().goToNextCell().run()">下一格</button>
-        <button @click="editor.chain().focus().goToPreviousCell().run()">上一格</button>
+      <div class="dropdown dropdown-top dropdown-open flex">
+        <div class="table-menus" contenteditable="false">
+          <button @click="focusedNode.addColumnBefore().run()">
+            <img src="../../assets/table.column.plus.before.svg" />
+          </button>
+          <button @click="focusedNode.addColumnAfter().run()">
+            <img src="../../assets/table.column.plus.after.svg" />
+          </button>
+          <button @click="focusedNode.deleteColumn().run()">
+            <img src="../../assets/table.column.remove.svg" />
+          </button>
+          <button @click="focusedNode.addRowBefore().run()">
+            <img src="../../assets/table.row.plus.before.svg" />
+          </button>
+          <button @click="focusedNode.addRowAfter().run()">
+            <img src="../../assets/table.row.plus.after.svg" />
+          </button>
+          <button @click="focusedNode.deleteRow().run()">
+            <img src="../../assets/table.row.remove.svg" />
+          </button>
+          <button @click="focusedNode.deleteTable().run()">
+            <img src="../../assets/table.remove.svg" />
+          </button>
+          <button @click="focusedNode.mergeCells().run()">
+            <img src="../../assets/table.cell.merge.svg" />
+          </button>
+          <button @click="focusedNode.splitCell().run()">
+            <img src="../../assets/table.cell.split.svg" />
+          </button>
+          <button @click="focusedNode.toggleHeaderColumn().run()">切换表头列</button>
+          <button @click="focusedNode.toggleHeaderRow().run()">切换表头行</button>
+          <button @click="focusedNode.toggleHeaderCell().run()">切换表头格</button>
+          <button @click="focusedNode.mergeOrSplit().run()">合并或拆分</button>
+          <button @click="focusedNode.setCellAttribute('colspan', 2).run()">占两格</button>
+          <button @click="focusedNode.fixTables().run()">修复</button>
+          <button @click="focusedNode.goToNextCell().run()">下一格</button>
+          <button @click="focusedNode.goToPreviousCell().run()">上一格</button>
+        </div>
       </div>
     </template>
 
@@ -51,6 +53,7 @@ import { nodeViewProps, NodeViewWrapper, NodeViewContent } from '@tiptap/vue-3'
 import { computed, onMounted, ref } from 'vue'
 
 const props = defineProps(nodeViewProps)
+const focusedNode = computed(() => props.editor.chain().focus())
 const shouldShow = ref(false)
 // 是否是整个editor.state.doc.content的最后一个node
 let isTheLastNode = computed(
@@ -83,10 +86,14 @@ onMounted(() => {
 
 <style lang="postcss">
 .table-menus {
-  @apply bg-success/90 text-info-content rounded-md px-2 py-1 flex flex-row flex-wrap;
+  @apply bg-success/90 text-info-content rounded-md px-2 py-1 flex flex-row flex-wrap dropdown-content;
 
   button {
     @apply btn btn-sm btn-ghost px-1;
+
+    img {
+      @apply w-6 h-6 m-0;
+    }
   }
 }
 
