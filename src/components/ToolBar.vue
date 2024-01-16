ee29cf506cba43f13d60ad1739b17b5bef915e7f
nooks <nooks@qq.com>
2024-01-16 20:58:30 +0800

    修复两个画图互相干扰的问题

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="flex flex-row container mx-auto justify-center mt-4 sticky top-4 join z-50" v-if="feature.toolbarVisible">
+  <div
+    class="flex flex-row container mx-auto justify-center mt-4 sticky top-4 join z-50"
+    v-if="feature.toolbarVisible"
+  >
     <button class="btn btn-primary btn-xs join-item" @click="app.setCurrentNode(Sample.sampleNode)">
       通用文章
+    </button>
+    <button class="btn btn-primary btn-xs join-item" @click="app.setCurrentNode(Sample.drawNode)">
+      画图文章
     </button>
     <button class="btn btn-primary btn-xs join-item" @click="app.setCurrentNode(Sample.tableNode)">
       表格文章
@@ -12,40 +18,75 @@
     <button class="btn btn-primary btn-xs join-item" @click="app.setCurrentNode(Sample.liteNode)">
       小型文章
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="app.setCurrentNode(Sample.nodeWithChildren)">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="app.setCurrentNode(Sample.nodeWithChildren)"
+    >
       带子节点文章
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.enableEdit" v-if="!feature.editable">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.enableEdit"
+      v-if="!feature.editable"
+    >
       编辑模式
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.disableEdit" v-if="feature.editable">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.disableEdit"
+      v-if="feature.editable"
+    >
       只读模式
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.enableTable" v-if="!feature.tableEnabled">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.enableTable"
+      v-if="!feature.tableEnabled"
+    >
       开启 Table
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.disableTable" v-if="feature.tableEnabled">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.disableTable"
+      v-if="feature.tableEnabled"
+    >
       关闭 Table
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.enableDraw" v-if="!feature.drawEnabled">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.enableDraw"
+      v-if="!feature.drawEnabled"
+    >
       开启 Draw
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.disableDraw" v-if="feature.drawEnabled">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.disableDraw"
+      v-if="feature.drawEnabled"
+    >
       关闭 Draw
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.showEditor" v-if="!feature.editorVisible">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.showEditor"
+      v-if="!feature.editorVisible"
+    >
       显示 Editor
     </button>
-    <button class="btn btn-primary btn-xs join-item" @click="feature.hideEditor" v-if="feature.editorVisible">
+    <button
+      class="btn btn-primary btn-xs join-item"
+      @click="feature.hideEditor"
+      v-if="feature.editorVisible"
+    >
       隐藏 Editor
     </button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useAppStore } from '../stores/AppStore';
+import { useAppStore } from '../stores/AppStore'
 import { useFeatureStore } from '../stores/FeatureStore'
-import Sample from '../entities/Sample';
+import Sample from '../entities/Sample'
 
 const feature = useFeatureStore()
 const app = useAppStore()
--- src/extensions/SmartDraw/SmartDraw.vue
+++ src/extensions/SmartDraw/SmartDraw.vue
@@ -10,21 +10,24 @@ import { ref } from 'vue'
 
 const img = ref(null)
 const props = defineProps(nodeViewProps)
+var iframe = document.createElement('iframe')
+var dialog = document.createElement('dialog')
 
-const closeListener = (_event: any) => {
+function closeListener(_event: any) {
   console.log('收到关闭画图的事件')
-  document.getElementsByTagName('iframe').item(0)?.contentWindow?.postMessage(JSON.stringify({ action: 'exit' }), '*');
-};
+  document
+    .getElementsByTagName('iframe')
+    .item(0)
+    ?.contentWindow?.postMessage(JSON.stringify({ action: 'exit' }), '*')
+}
 
-const showIframe = function () {
+function showIframe() {
   if (!props.editor.isEditable) {
     return
   }
 
-  const dialog = document.createElement('dialog')
   dialog.classList.add('modal')
 
-  const iframe = document.createElement('iframe')
   iframe.setAttribute('frameborder', '0')
   iframe.setAttribute('src', props.extension.options.drawIoLink)
   iframe.setAttribute('width', '100%')
@@ -34,18 +37,20 @@ const showIframe = function () {
   document.body.appendChild(dialog)
 
   dialog.showModal()
-  window.addEventListener('message', (event: MessageEvent) => {
-    receive(event, iframe, dialog)
-  })
+  window.addEventListener('message', receive)
 
   document.addEventListener('close-draw', closeListener)
 }
 
-const receive = function (
-  event: MessageEvent,
-  iframe: HTMLIFrameElement,
-  dialog: HTMLDialogElement
-): void {
+function destroyIframe(dialog: HTMLDialogElement) {
+  console.log('SmartDraw: 销毁画图的 Iframe，同时取消事件监听')
+  window.removeEventListener('message', receive)
+  document.removeEventListener('close-draw', closeListener)
+  document.body.removeChild(dialog)
+  dialog.close()
+}
+
+function receive(event: MessageEvent): void {
   const source = img.value as unknown as HTMLElement
   if (event.data.length == 0) {
     return
@@ -67,7 +72,7 @@ const receive = function (
       )
       break
     case 'save':
-      console.log('收到事件', msg)
+      console.log('SmartDraw: 收到 Save 事件，表示在画图 Iframe 中点击了保存')
       iframe.contentWindow!.postMessage(
         JSON.stringify({
           action: 'export',
@@ -76,25 +81,19 @@ const receive = function (
         }),
         '*'
       )
-
       break
     case 'export':
-      console.log('收到事件', msg)
+      console.log('SmartDraw: 收到 Export 事件，解析并存储数据')
       props.updateAttributes({
         src: msg.data
       })
-      window.removeEventListener('message', receive)
-      document.body.removeChild(dialog)
-      dialog.close()
+      destroyIframe(dialog)
       break
     case 'exit':
-      window.removeEventListener('message', receive)
-      document.removeEventListener('close-draw', closeListener)
-      document.body.removeChild(dialog)
-      dialog.close()
+      console.log('SmartDraw: 收到 Exit 事件，表示画图 Iframe 已退出')
       break
     case 'load':
-      console.log('收到事件', msg)
+      console.log('SmartDraw: 收到 Load 事件，表示画图 Iframe 已加载')
   }
 }
 </script>
