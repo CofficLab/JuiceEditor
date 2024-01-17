a03a85063e8339ee726f392730b4744f4c564999
nookery <nooks@qq.com>
2024-01-17 09:17:29 +0800

    Update DrawAgent.ts

--- src/entities/DrawAgent.ts
+++ src/entities/DrawAgent.ts
@@ -7,18 +7,23 @@ export default class DrawAgent {
         const query = SmartTool.httpBuildQuery({
             embed: '1',
             ui: 'min',
-            spin: '1',
             modified: 'unsavedChanges',
             proto: 'json',
             lang: 'zh',
             'hide-pages': '1',
             lightbox: '0',
             browser: '0',
+            // Disables the Google integration
             gapi: '0',
+            // Disables the Dropbox integration
             db: '0',
+            // Disables the OneDrive integration
             od: '0',
+            // Disables the Trello integration
             tr: '0',
+            // Disables the GitHub integration
             gh: '0',
+            // Disables the GitLab integration
             gl: '0',
             noSaveBtn: '0',
             noExitBtn: '1',
