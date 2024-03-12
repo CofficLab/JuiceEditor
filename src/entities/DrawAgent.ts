607cc416d04da73bfc2e6074555fbfca900c3e61
nooks <nooks@qq.com>
2024-03-12 21:15:04 +0800

    Update DrawAgent.ts

--- src/entities/DrawAgent.ts
+++ src/entities/DrawAgent.ts
@@ -4,6 +4,6 @@ export default class DrawAgent {
     static getLink() {
         return isDebug
             ? 'http://localhost:5173/drawio/webapp/index.html?'
-            : 'http://localhost:49493/draw/index.html?'
+            : 'http://localhost:49493/draw/webapp/index.html?'
     }
 }
