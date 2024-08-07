208ab8cb82ae257c23d6c4fa6b0e48085fd753e0
Nookery <nooks@qq.com>
2024-08-07 21:53:30 +0800

    🐛 Fix a bug

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ const isDebug = process.env.NODE_ENV === 'development'
 export const Config = {
     'isDebug': isDebug,
     'drawLink': isDebug
-        ? 'http://localhost:5173/drawio/webapp/index.html?'
+        ? '/drawio/webapp/index.html?'
         : '/drawio/index.html?',
     'monacoLink': isDebug
         ? '/monaco/index.html'
