9b1a4d798456fcf0e8ea5288aab7e4b0b770a81d
Nookery <nooks@qq.com>
2024-08-09 23:41:44 +0800

    Update ButtonBar.vue

--- src/ui/ButtonBar.vue
+++ src/ui/ButtonBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-row items-center shadow-lg bg-slate-500/20 border border-green-800/40">
+  <div
+    class="flex flex-row items-center border shadow-lg bg-slate-500/20 border-green-800/40 not-prose"
+  >
     <slot></slot>
   </div>
 </template>
