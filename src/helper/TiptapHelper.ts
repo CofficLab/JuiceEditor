b68e49d0de471951189999abcba16dbd48c28f48
Nookery <nooks@qq.com>
2024-08-10 21:39:06 +0800

    Update TiptapHelper.ts

--- src/helper/TiptapHelper.ts
+++ src/helper/TiptapHelper.ts
@@ -196,8 +196,12 @@ class TiptapHelper {
     // 在本节点的后面插入一行
     static newLineOf(editor: TiptapEditor, node: ProseMirrorNode, pos: number) {
         let tail = TiptapHelper.getTailPosOf(editor, node, pos)
+        console.log('new line, node is', node)
+        console.log('new line, pos is', pos)
+        console.log('node size is', node.nodeSize)
         console.log('tail is', tail)
-        editor.commands.insertContentAt(tail - 1, '<p>type here...</p>', {
+        // YmdHis格式的时间
+        editor.commands.insertContentAt(tail, '<p>type here...' + new Date() + '</p>', {
             updateSelection: false,
             parseOptions: {
                 preserveWhitespace: 'full'
