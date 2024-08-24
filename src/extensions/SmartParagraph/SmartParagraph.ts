8151ed7c0969a882169d7b22ad59d8dcab84347d
Nookery <nooks@qq.com>
2024-08-24 20:01:43 +0800

    🐎 Enhance existing functionality

--- src/extensions/SmartParagraph/SmartParagraph.ts
+++ src/extensions/SmartParagraph/SmartParagraph.ts
@@ -1,6 +1,7 @@
 import { VueNodeViewRenderer } from "@tiptap/vue-3";
 import SmartParagraphVue from "./SmartParagraph.vue";
 import Paragraph from "@tiptap/extension-paragraph";
+import { v4 as uuidv4 } from 'uuid';
 
 const SmartParagraph = Paragraph.extend({
     parseHTML: () => [
@@ -15,6 +16,16 @@ const SmartParagraph = Paragraph.extend({
             },
             type: {
                 default: "paragraph"
+            },
+            uuid: {
+                default: null,
+                parseHTML: element => element.getAttribute('data-uuid'),
+                renderHTML: attributes => {
+                    if (!attributes.uuid) {
+                        attributes.uuid = uuidv4();
+                    }
+                    return { 'data-uuid': attributes.uuid }
+                }
             }
         }
     },
