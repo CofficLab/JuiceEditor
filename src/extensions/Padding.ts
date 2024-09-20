2b24907b32db3a541b8c2d26b3f1239677dcd4b0
Nookery <nooks@qq.com>
2024-09-20 17:59:01 +0800

    Create x.html

--- src/extensions/Padding.ts
+++ src/extensions/Padding.ts
@@ -1,12 +1,19 @@
 import { Extension } from '@tiptap/core';
 import { Plugin, PluginKey } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
-import { IMAGE, PRE } from '../config/nodes';
+import { IMAGE, PRE, ROOT } from '../config/nodes';
 
-const noPaddingTypes = [PRE, IMAGE];
+const noPaddingTypes = [PRE, IMAGE, ROOT];
+const title = '👔 Padding'
 
 export const Padding = Extension.create({
     name: 'padding',
+
+    addOptions() {
+        return {
+            noPaddingTypes: noPaddingTypes,
+        }
+    },
 
     addProseMirrorPlugins() {
         return [
@@ -17,15 +24,24 @@ export const Padding = Extension.create({
                         const decorations: Decoration[] = []
 
                         doc.descendants((node, pos) => {
+                            let verbose = false
+
                             if (node.isInline || noPaddingTypes.includes(node.type.name) || doc.resolve(pos).depth > 1) {
-                                return false
+                                if (verbose) {
+                                    console.log(title, "no padding for", node.type.name)
+                                }
+                            } else {
+                                if (verbose) {
+                                    console.log(title, "padding for", node.type.name)
+                                }
+
+                                decorations.push(
+                                    Decoration.node(pos, pos + node.nodeSize, {
+                                        class: 'px-8',
+                                    }),
+                                )
                             }
 
-                            decorations.push(
-                                Decoration.node(pos, pos + node.nodeSize, {
-                                    class: 'px-8',
-                                }),
-                            )
                         })
 
                         return DecorationSet.create(doc, decorations)
