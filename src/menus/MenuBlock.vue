065d3a38766875377306aaca855a3f72e75e3e1a
Nookery <nooks@qq.com>
2024-10-22 23:01:07 +0800

    💄 UI:  Update user interface elements

--- src/menus/MenuBlock.vue
+++ src/menus/MenuBlock.vue
@@ -153,35 +153,29 @@ function shouldShowNewLineMenu() {
 				<RiIndentIncrease :size="iconSize"></RiIndentIncrease>
 			</Button>
 
-			<div class="dropdown dropdown-bottom h-8 w-8">
-				<Button tabindex="0" role="button" tips="样式" size="md" :shape="shape">
-					<RiPaletteLine :size="iconSize"></RiPaletteLine>
-				</Button>
-				<div tabindex="0"
-					class="dropdown-content bg-slate-100 dark:bg-zinc-900 rounded-box z-50 p-2 shadow w-48">
-					<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-2">
-						<div v-for="color in Object.keys(colorClass)"
-							class="w-7 h-7 flex items-center justify-center cursor-pointer transition-colors duration-200 ease-in-out hover:bg-indigo-200/90 rounded-full p-1"
-							:key="color" @click="props.editor.commands.setBackgroundColor(color)">
-							<div :class="colorClass[color]" class="w-5 h-5 rounded-full p-1"></div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<!-- Menu for paragraph -->
 
-			<div class="dropdown dropdown-bottom h-8 w-8">
-				<Button tabindex="0" role="button" tips="翻译" size="md" :shape="shape">
-					<RiGlobalLine :size="iconSize"></RiGlobalLine>
-				</Button>
-				<div tabindex="0"
-					class="dropdown-content bg-slate-100 dark:bg-zinc-900 rounded-box z-50 p-2 shadow w-48">
-					<div class="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-2">
-						<div v-for="language in languages" @click="props.editor.commands.translate(language)">
-							<Button size="xl" shape="square">{{ language }}</Button>
-						</div>
+			<Button tips="样式" :shape="shape" v-if="shouldShowParagraphMenu()">
+				<RiPaletteLine :size="iconSize"></RiPaletteLine>
+
+				<template #dropdown-item>
+					<div v-for="color in Object.keys(colorClass)"
+						class="w-7 h-7 flex items-center justify-center cursor-pointer transition-colors duration-200 ease-in-out hover:bg-indigo-200/90 rounded-full p-1"
+						:key="color" @click="props.editor.commands.setBackgroundColor(color)">
+						<div :class="colorClass[color]" class="w-5 h-5 rounded-full p-1"></div>
 					</div>
-				</div>
-			</div>
+				</template>
+			</Button>
+
+			<Button tips="翻译" :shape="shape" v-if="shouldShowParagraphMenu()">
+				<RiGlobalLine :size="iconSize"></RiGlobalLine>
+
+				<template #dropdown-item>
+					<div v-for="language in languages" @click="props.editor.commands.translate(language)">
+						<Button size="xl" shape="square">{{ language }}</Button>
+					</div>
+				</template>
+			</Button>
 		</ButtonList>
 	</div>
 </template>
--- src/ui/Button.vue
+++ src/ui/Button.vue
@@ -2,6 +2,9 @@
 import { ref, computed } from 'vue'
 
 const showTips = ref(false)
+const hovered = ref(false)
+const dropdownHovered = ref(false)
+const showDropdown = ref(false)
 
 const props = defineProps({
   size: {
@@ -31,6 +34,34 @@ const sizeClass = computed(() => {
     default: return 'h-10 w-10'
   }
 })
+
+function handleMouseover() {
+  hovered.value = true
+  showTips.value = true
+  showDropdown.value = true
+}
+
+function handleMouseleave() {
+  hovered.value = false
+  showTips.value = false
+  setTimeout(() => {
+    if (!dropdownHovered.value) {
+      showDropdown.value = false
+    }
+  }, 500)
+}
+
+function handleDropdownMouseover() {
+  hovered.value = true
+  showDropdown.value = true
+  dropdownHovered.value = true
+}
+
+function handleDropdownMouseleave() {
+  hovered.value = false
+  showDropdown.value = false
+  dropdownHovered.value = false
+}
 </script>
 
 <template>
@@ -39,7 +70,7 @@ const sizeClass = computed(() => {
       class="absolute px-2 mb-1 text-xs text-white bg-gray-700 rounded group-hover:block bottom-full whitespace-nowrap"
       v-text="props.tips" v-if="showTips && props.tips"></div>
 
-    <button @mouseover="showTips = true" @mouseleave="showTips = false" :class="{
+    <button @mouseover="handleMouseover" @mouseleave="handleMouseleave" :class="{
       'h-full w-full px-0 items-center flex justify-center': true,
       'text-xs': props.size === 'xs',
       'text-sm': props.size === 'sm',
@@ -54,5 +85,11 @@ const sizeClass = computed(() => {
     }">
       <slot></slot>
     </button>
+
+    <div v-if="showDropdown && $slots['dropdown-item']" @mouseover="handleDropdownMouseover"
+      @mouseleave="handleDropdownMouseleave"
+      class="bg-slate-100 dark:bg-zinc-900 rounded-box z-50 p-2 shadow flex flex-row w-72 h-72">
+      <slot name="dropdown-item"></slot>
+    </div>
   </div>
 </template>
