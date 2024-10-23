41537b2721e444281fa8cd8a93b47a7b47fe33e9
Nookery <nooks@qq.com>
2024-10-23 09:32:34 +0800

    💄 UI:  Update user interface elements

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -27,8 +27,12 @@ const languages = [
     '日语',
 ]
 
-const menuClass = 'bg-slate-100 px-4 py-2 h-10 flex flex-row shadow gap-2 justify-center items-center rounded-2xl'
+const menuClass = 'bg-slate-300/50 px-4 py-2 h-10 flex flex-row shadow gap-2 justify-center items-center rounded-2xl'
 const activeMenuClass = 'bg-slate-200 justify-center items-center rounded-2xl p-1'
+const cardClass1 = 'bg-indigo-100/30 rounded-2xl p-4 shadow-xl'
+const cardClass2 = 'bg-cyan-100/30 rounded-2xl p-4 shadow-2xl'
+const cardClass3 = 'bg-orange-100/30 rounded-2xl p-4 shadow-2xl'
+const cardClass4 = 'bg-green-100/30 rounded-2xl p-4 shadow-2xl'
 
 const iconSize = '24px'
 
@@ -52,13 +56,13 @@ function downloadAsImage() {
 
 
 <template>
-    <div class="h-full min-h-screen w-full bg-gray-500/20 mt-24 p-4 glass">
+    <div class="h-full min-h-screen w-full bg-gray-500/20 mt-24 p-4 text-slate-700">
         <div class="flex flex-row justify-center mb-8">
-            <button @click="downloadAsImage" class="mt-4 p-2 bg-blue-500 text-white rounded">Download as Image</button>
+            <button @click="downloadAsImage" class="mt-4 p-2 bg-blue-500/50 rounded">Download as Image</button>
         </div>
 
         <div id="box" class="max-w-4xl mx-auto p-8">
-            <div id="section1" class="bg-indigo-500/10 rounded-2xl p-4 shadow-2xl">
+            <div id="section1" :class="cardClass1">
                 <div :class="menuClass" class="w-36 ml-24">
                     <RiH1 :size="iconSize" color="primary" />
                     <RiH2 :size="iconSize" color="primary" />
@@ -83,7 +87,7 @@ function downloadAsImage() {
             </div>
 
             <div id="section2" class="flex flex-row bg-indigo-500/0 mt-4 rounded-2xl gap-2 justify-between">
-                <div id="image" class="rounded-xl bg-rose-400/30 p-4 shadow-2xl">
+                <div id="image" :class="cardClass2">
                     <div :class="menuClass" class="rounded-box z-50 shadow w-40 ml-40 mb-1 flex flex-row gap-2">
                         <div :class="shape" v-for="shape in shapeClass" class="w-5 h-5 rounded-full p-1"></div>
                     </div>
@@ -103,7 +107,7 @@ function downloadAsImage() {
                 </div>
 
                 <div id="link-list" class="flex flex-col gap-2 justify-between">
-                    <div id="link" class="rounded-xl p-4 bg-orange-300/40 shadow-2xl">
+                    <div id="link" :class="cardClass3">
                         <div :class="menuClass" class="w-72 ml-24">
                             <RiLink color="primary" />
                             <input type="text" value="https://cofficlab.github.io"
@@ -114,7 +118,7 @@ function downloadAsImage() {
                             <p>Create softwares with love by <b class="ring-1 rounded-2xl p-1">CofficLab</b></p>
                         </div>
                     </div>
-                    <div id="list" class="mt-8 rounded-xl p-4 bg-cyan-500/30 shadow-2xl">
+                    <div id="list" :class="cardClass4">
                         <div :class="menuClass" class="w-48">
                             <RiText color="secondary" />
                             <div v-for="language in languages" class="text-xs w-12 rounded-full p-1">{{ language }}
