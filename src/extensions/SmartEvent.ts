221c19d05befcaaf06674433d3e595324fc7a48b
Nookery <nooks@qq.com>
2024-10-24 23:01:49 +0800

    🐎 Improve:  Enhance existing functionality

--- src/extensions/SmartEvent.ts
+++ src/extensions/SmartEvent.ts
@@ -1,6 +1,11 @@
 import { Extension } from '@tiptap/core'
 import { Editor } from '@tiptap/core'
 import SmartParagraph from './SmartParagraph'
+import { Slice } from 'prosemirror-model'
+
+export enum EditorErrorType {
+    ContentError = 'content:error',
+}
 
 export enum SmartEventName {
     TranslationError = 'translation:error',
@@ -45,6 +50,53 @@ export const SmartEvent = Extension.create({
             }
         }
     },
+
+    onBeforeCreate() {
+        // Before the view is created.
+        console.log('onBeforeCreate')
+    },
+
+    onCreate() {
+        // The editor is ready.
+        console.log('onCreate')
+    },
+    onUpdate() {
+        // The content has changed.
+        console.log('onUpdate')
+    },
+    onSelectionUpdate() {
+        // The selection has changed.
+        console.log('onSelectionUpdate')
+    },
+    onTransaction() {
+        // The editor state has changed.
+        console.log('onTransaction')
+    },
+    onFocus() {
+        // The editor is focused.
+        console.log('onFocus')
+    },
+    onBlur() {
+        // The editor isn’t focused anymore.
+        console.log('onBlur')
+    },
+    onDestroy() {
+        // The editor is being destroyed.
+        console.log('onDestroy')
+    },
+    onPaste(event: ClipboardEvent, slice: Slice) {
+        // The editor is being pasted into.
+        console.log('onPaste', event, slice)
+    },
+    onDrop(event: DragEvent, slice: Slice, moved: boolean) {
+        // The editor is being pasted into.
+        console.log('onDrop', event, slice, moved)
+    },
+    onContentError({ editor, error, disableCollaboration }: { editor: Editor; error: Error; disableCollaboration: () => void }) {
+        // The editor content does not match the schema.
+        console.log('onContentError', editor, error, disableCollaboration)
+    },
+
 })
 
 function getDebugInfos(name: SmartEventName, editor: Editor) {
